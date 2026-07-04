e44cadc305b403fa671bf5eb2ee466e78fcd4d6a
virtualbox <virtualbox@gmail.com>
2026-07-05 02:12:21 +0300

    add test user

---
+++ tests/test_users.py
@@ -0,0 +1,56 @@
+import pytest
+from django.test import Client
+from apps.users.models import User
+
+@pytest.mark.django_db
+def test_register_success():
+    client = Client()
+
+    response = client.post(
+        "/api/users/register",
+        data={"username": "testuser", "password": "123456"},
+        content_type="application/json"
+    )
+
+    assert response.status_code == 200
+    assert "token" in response.json()
+
+@pytest.mark.django_db
+def test_register_duplicate_username():
+    User.objects.create_user(username="testuser", password="123456")
+
+    client = Client()
+    response = client.post(
+        "/api/users/register",
+        data={"username": "testuser", "password": "123456"},
+        content_type="application/json"
+    )
+
+    assert response.status_code == 400
+
+@pytest.mark.django_db
+def test_login_success():
+    user = User.objects.create_user(username="testuser", password="123456")
+    client = Client()
+
+    response = client.post(
+        "/api/users/login",
+        data={"username": "testuser", "password": "123456"},
+        content_type="application/json"
+    )
+
+    assert response.status_code == 200
+    assert "token" in response.json()
+
+
+@pytest.mark.django_db
+def test_login_invalid_credentials():
+    client = Client()
+
+    response = client.post(
+        "/api/users/login",
+        data={"username": "nope", "password": "wrong"},
+        content_type="application/json"
+    )
+
+    assert response.status_code == 401
